30daa9fbe3c3dfd71409e7c9d5126b4a2c6c90a1
JulianaW18 <juuhwalker18@gmail.com>
2018-10-30 22:01:42 -0300

    primeira aula de java

---
+++ script.js
@@ -0,0 +1,17 @@
+// let nome = "JulianaW";
+// let idade = 19;
+// let vivo = false;
+
+// let saida1 = "Olá, " + nome + "! Turubom?";
+// saida1 = `Olá, ${nome}! Você tem ${idade} anos!`;
+
+// console.log(saida1)
+// console.log(prompt("Qual seu nome?"));
+
+let dataDeNascimento = 1999;
+let ano = 2018;
+
+prompt(`Olá, que ano vc nasceu?`);
+console.log
+prompt(`em que ano estamos?`);
+alert(dataDeNascimento - ano);
